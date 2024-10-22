dab1120c8bd94c66523b238cdc5de921f9fcc41f
Evan Wiedemeier <114448825+eman60@users.noreply.github.com>
2024-10-22 17:13:39 -0500

    Made updates to mux control following PC add

--- project/demo1/verilog/execute.v
+++ project/demo1/verilog/execute.v
@@ -5,11 +5,11 @@
    Description     : This is the overall module for the execute stage of the processor.
 */
 `default_nettype none
-module execute (SLBIsel, incPC, immSrcl, imm8, imm11, brchSig, Cin, inA, inB, invA, invB, wrtData, aluOp, aluJmp, aluOut, newPC, pcAdd);
+module execute (SLBIsel, incPC, immSrc, imm8, imm11, brchSig, Cin, inA, inB, invA, invB, aluOp, aluJmp, jalSel, aluOut, newPC, pcAdd, wrtData);
 
    input SLBIsel;
    input incPC;
-   input immSrcl;
+   input immSrc;
    input [15:0] imm8;
    input [15:0] imm11;
    input [1:0] brchSig;
@@ -18,36 +18,38 @@ module execute (SLBIsel, incPC, immSrcl, imm8, imm11, brchSig, Cin, inA, inB, in
    input [15:0] inB;
    input invA;
    input invB;
-   input [15:0] wrtData;
    input [3:0] aluOp;
    input aluJmp;
+   input jalSel;
 
    output [15:0] aluOut;
    output [15:0] newPC;
-   output [15:0] pcAdd;
+   output [15:0] addPC;
+   wire [15:0] wrtData;
 
    wire zeroFlag;
    wire oflFlag;
    wire jmpSel;
    wire [15:0] pcOrSLBI;
    wire [15:0] imm8Or11;
-   wire [15:0] addPC;
+   wire [15:0] pcAdd;
 
-
-
-
+   // ALU
    alu aluExec(.InA(inA), .InB(inB), .Cin(Cin), .Oper(aluOp), .invA(invA), .invB(invB), .sign(1'b0), .Out(aluOut), .Zero(zeroFlag), .Ofl(oflFlag));
 
-   branch_conditional branchCond(.in(brchSig), .sf(), .zf(zeroFlag), .of(oflFlag), .cf(), .out(jmpSel));
+   // Branch conditional module
+   branch_conditional branchCond(.brchSig(brchSig), .sf(), .zf(zeroFlag), .of(oflFlag), .cf(), .jmpSel(jmpSel));
 
    assign pcOrSLBI = (SLBIsel) ? aluOut : incPC;
-   assign imm8Or11 = (immSrcl) ? imm11 : imm8;
+   assign imm8or11 = (immSrc) ? imm11 : imm8;
 
-   cla_16 pcImmAdd(.sum(addPC), .cout(), .ofl(), .a(pcOrSLBI), .b(imm8Or11), .c_in(1'b0), .sign(1'b0));
+   // PC add module 
+   cla_16 pcImmAdd(.sum(addPC), .cout(), .ofl(), .a(pcOrSLBI), .b(imm8or11), .c_in(1'b0), .sign(1'b0));
    
+   // 2:1 muxes to control PC and register wb values
    assign pcAdd = (jmpSel) ? addPC : incPC;
-
    assign newPC = (aluJmp) ? aluOut : pcAdd;
+   assign addPC = (jalSel)
 
 endmodule
 `default_nettype wire
