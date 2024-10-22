6bb9b16e3b53a61812b32bc1859cf144226989e6
Evan Wiedemeier <114448825+eman60@users.noreply.github.com>
2024-10-22 15:27:24 -0500

    Fixed compilation errors in decode module

--- project/demo1/verilog/decode.v
+++ project/demo1/verilog/decode.v
@@ -7,34 +7,34 @@
 `default_nettype none
 module decode (instruction, wbData, clk, rst, imm8, imm11, ALUjmp, SLBIsel, createDump, memWrt, brchSig, Cin, invA, invB, wbDataSel, immSrc, aluOp, inA, inB, wrtData, err);
 
-input [15:0] instruction;
-input [15:0] wbData;
-input clk;
-input rst;
+input wire [15:0] instruction;
+input wire [15:0] wbData;
+input wire clk;
+input wire rst;
 
 // immediate outputs
-output [15:0] imm8;
-output [15:0] imm11;
+output wire [15:0] imm8;
+output wire [15:0] imm11;
 
 // from instruction decoder 
-output ALUjmp;
-output SLBIsel;
-output createDump;
-output memWrt;           // memory write or read signal 
-output [1:0] brchSig;
-output Cin;
-output invA;             // invert ALU input A
-output invB;             // invert ALU input B
-output [1:0] wbDataSel;           // choose source of writeback data 
-output immSrc;                    // used to choose which immediate to add to PC
-output aluOp;                     // signal to ALU to choose operation
+output wire ALUjmp;
+output wire SLBIsel;
+output wire createDump;
+output wire memWrt;           // memory write or read signal 
+output wire [1:0] brchSig;
+output wire Cin;
+output wire invA;             // invert ALU input A
+output wire invB;             // invert ALU input B
+output wire [1:0] wbDataSel;           // choose source of writeback data 
+output wire immSrc;                    // used to choose which immediate to add to PC
+output wire aluOp;                     // signal to ALU to choose operation
 
 // from register file / reg mux 
-output [15:0] inA;
-output [15:0] inB;
+output wire [15:0] inA;
+output wire [15:0] inB;
 
-output [15:0] wrtData;
-output err;
+output wire [15:0] wrtData;
+output wire err;
 
 wire zeroSel;            // choose zero or sign extended immediates 
 wire [15:0] imm5;
@@ -53,7 +53,7 @@ assign imm5 = (zeroSel) ? {11'b0 , instruction[4:0]} : {{11 {instruction[4]}} , 
 assign imm8 = (zeroSel) ? {8'b0 , instruction[7:0]} : {{8 {instruction[7]}} , instruction[7:0]};
 
 // sign extend 11 bit immediate 
-assign imm11 = {{5 {instruction[10]}} , instruction[10:0]}
+assign imm11 = {{5 {instruction[10]}} , instruction[10:0]};
 
 // module to decode instructions to ALU operations   ~~~~~~~~~~~~~~~~~~HAVE TO IMPLEMENT~~~~~~~~~~~~~~~~~~~~
 alu_op_decode ALU_OP(.instruction(instruction[15:0]), .aluOp(aluOp));
@@ -68,7 +68,7 @@ regFile_bypass register_file(.read1Data(inA), .read2Data(regB), .err(err), .clk(
 assign inB = (BSrc == 2'b0) ? regB : (BSrc == 2'b01) ? imm5 : (BSrc == 2'b10) ? imm11 : 16'b0;
 
 // instruction decoder. ~~~~~~~~~~~~~~~~~~HAVE TO IMPLEMENT~~~~~~~~~~~~~~~~~~~~
-control_unit instruction_decoder(.instruction(instruction), .ALUjmp(ALUjmp), .memWrt(memWrt), .brchSig(brchSig), .Cin(Cin), .invA(invA), .invB(invB), .BSrc(BSrc), .regWrt(regWrt), .wbDataSel(wbDataSel), .stuSel(stuSel), .immSrc(immSrc), .SLBIsel(SLBIsel), .createDump(createDump), .BSrc(BSrc), .zeroSel(zeroSel), .regDestSel(regDestSel));
+control_unit instruction_decoder(.instruction(instruction), .ALUjmp(ALUjmp), .memWrt(memWrt), .brchSig(brchSig), .Cin(Cin), .invA(invA), .invB(invB), .regWrt(regWrt), .wbDataSel(wbDataSel), .stuSel(stuSel), .immSrc(immSrc), .SLBIsel(SLBIsel), .createDump(createDump), .BSrc(BSrc), .zeroSel(zeroSel), .regDestSel(regDestSel));
    
 endmodule
 `default_nettype wire
