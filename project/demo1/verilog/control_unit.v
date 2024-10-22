93a977a92026d6337f536cb2b40d7442e19377ca
agsiv25 <sivarajah@wisc.edu>
2024-10-22 00:50:04 -0500

    execute (partial), control unit(partial)

--- project/demo1/verilog/control_unit.v
+++ project/demo1/verilog/control_unit.v
@@ -28,6 +28,129 @@ output zeroSel;             // choose zero or sign extended immediates
 output [1:0] regDestSel;    // sel signal to register write mux 
 
 // IMPLEMENT HERE 
+always @(instruction) begin
+   ALUjmp = 1'b0;
+   ALUOpr = 5'b0;
+   memWrt = 1'b0;
+   brchSig = 2'b00;
+   Cin = 1'b0;
+   invA = 1'b0;
+   invB = 1'b0;
+   BSrc = 2'b00;
+   regWrt = 1'b0;
+   wbDataSel = 2'b00;
+   stuSel = 1'b0;
+   immSrc = 1'b0;
+   SLBIsel = 1'b0;
+   createDump = 1'b0;
+   BSrc = 2'b00;
+   zeroSel = 1'b0;
+   regDestSel = 2'b00;
+   casex(instruction[15:11])
+      0_0000: begin // HALT
+
+      end
+      0_0001: begin // NOP
+
+      end
+      0_1000: begin //ADDI
+
+      end
+      0_1001: begin //SUBI
+
+      end
+      0_1010: begin //XORI
+
+      end
+      0_1011: begin //ANDNI
+
+      end
+      1_0100: begin //ROLI
+
+      end
+      1_0101: begin //SLLI
+
+      end
+      1_0110: begin //RORI
+
+      end
+      1_0111: begin //SRLI
+
+      end
+      1_0000: begin //ST
+
+      end
+      1_0001: begin //LD
+
+      end
+      1_0011: begin //STU
+
+      end
+      1_1001: begin //BTR
+
+      end
+      1_1011: begin //ALU (ADD, SUB, XOR, ANDN)
+
+      end
+      1_1010: begin //ALU (ROL, SLL, ROR, SRL)
+
+      end
+      1_1100: begin //SEQ
+
+      end
+      1_1101: begin //SLT
+
+      end
+      1_1110: begin //SLE
+
+      end
+      1_1111: begin //SCO
+
+      end
+      0_1100: begin //BEQZ
+
+      end
+      0_1101: begin //BNEZ
+
+      end
+      0_1110: begin //BLTZ
+
+      end
+      0_1111: begin //BGEZ
+
+      end
+      1_1000: begin //LBI
+
+      end
+      1_0010: begin //SLBI
+
+      end
+      0_0100: begin //J
+
+      end
+      0_0101: begin //JR
+
+      end
+      0_0110: begin //JAL
+
+      end
+      0_0111: begin //JALR
+
+      end
+      0_0010: begin //siic
+
+      end
+      0_0011: begin //NOP/RTI
+
+      end
+      default: begin
+         err = 1'b1;
+      end
+   endcase
+end
+
+        
+         
 
 endmodule
 `default_nettype wire
--- project/demo1/verilog/execute.v
+++ project/demo1/verilog/execute.v
@@ -5,9 +5,49 @@
    Description     : This is the overall module for the execute stage of the processor.
 */
 `default_nettype none
-module execute (/* TODO: Add appropriate inputs/outputs for your execute stage here*/);
+module execute (SLBIsel, incPC, immSrcl, imm8, imm11, brchSig, Cin, inA, inB, invA, invB, wrtData, aluOp, aluJmp, aluOut, newPC, pcAdd);
 
-   // TODO: Your code here
+   input SLBIsel;
+   input incPC;
+   input immSrcl;
+   input [15:0] imm8;
+   input [15:0] imm11;
+   input [1:0] brchSig;
+   input Cin;
+   input [15:0] inA;
+   input [15:0] inB;
+   input invA;
+   input invB;
+   input [15:0] wrtData;
+   input [3:0] aluOp;
+   input aluJmp;
+
+   output [15:0] aluOut;
+   output [15:0] newPC;
+   output [15:0] pcAdd;
+
+   wire zeroFlag;
+   wire oflFlag;
+   wire jmpSel;
+   wire [15:0] pcOrSLBI;
+   wire [15:0] imm8Or11;
+   wire [15:0] addPC;
+
+
+
+
+   alu aluExec(.InA(inA), .InB(inB), .Cin(Cin), .Oper(aluOp), .invA(invA), .invB(invB), .sign(1'b0), .Out(aluOut), .Zero(zeroFlag), .Ofl(oflFlag));
+
+   branch_conditional branchCond(.in(brchSig), .sf(), .zf(zeroFlag), .of(oflFlag), .cf(), .out(jmpSel));
+
+   assign pcOrSLBI = (SLBIsel) ? aluOut : incPC;
+   assign imm8Or11 = (immSrcl) ? imm11 : imm8;
+
+   cla_16 pcImmAdd(.sum(addPC), .cout(), .ofl(), .a(pcOrSLBI), .b(imm8Or11), .c_in(1'b0), .sign(1'b0));
    
+   assign pcAdd = (jmpSel) ? addPC : incPC;
+
+   assign newPC = (aluJmp) ? aluOut : pcAdd;
+
 endmodule
 `default_nettype wire
