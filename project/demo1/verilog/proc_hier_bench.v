bef2ae3f75e8bd69bbef24c7b2ea033a8e065a5a
eman60 <evanwiedemeier@gmail.com>
2024-10-22 21:22:07 -0500

    change testbench signal names

--- project/demo1/verilog/proc_hier_bench.v
+++ project/demo1/verilog/proc_hier_bench.v
@@ -125,32 +125,32 @@ module proc_hier_bench();
    // Edit the example below. You must change the signal
    // names on the right hand side
     
-   assign PC = DUT.p0.fetch0.newPC;
-   assign Inst = DUT.p0.fetch0.instruction;
+   assign PC = DUT.p0.fetchSection.newPC;
+   assign Inst = DUT.p0.fetchSection.instruction;
    
-   assign RegWrite = DUT.p0.decode0.regFile0.regWrt;
+   assign RegWrite = DUT.p0.decodeSection.register_file.regWrt;
    // Is register being written, one bit signal (1 means yes, 0 means no)
    
-   assign WriteRegister = DUT.p0.decode0.regFile0.wrtReg;
+   assign WriteRegister = DUT.p0.decodeSection.register_file.wrtReg;
    // The name of the register being written to. (3 bit signal)
 
-   assign WriteData = DUT.p0.decode0.regFile0.w;
+   assign WriteData = DUT.p0.decodeSection.register_file.w;
    // Data being written to the register. (16 bits)
    
-   assign MemRead =  ~DUT.p0.memory0.memWrt;
+   assign MemRead =  ~DUT.p0.memorySection.memWrt;
    // Is memory being read, one bit signal (1 means yes, 0 means no)
 
    //assign MemWrite = (DUT.p0.memory0.memReadorWrite & DUT.p0.memory0.memWrite);
-   assign MemWrite = (DUT.p0.memory0.memWrt);
+   assign MemWrite = (DUT.p0.memorySection.memWrt);
    // Is memory being written to (1 bit signal)
    
-   assign MemAddress = DUT.p0.memory0.dataAddr;
+   assign MemAddress = DUT.p0.memorySection.dataAddr;
    // Address to access memory with (for both reads and writes to memory, 16 bits)
    
-   assign MemData = DUT.p0.memory0.wrtData;
+   assign MemData = DUT.p0.memorySection.wrtData;
    // Data to be written to memory for memory writes (16 bits)
    
-   assign Halt = DUT.p0.memory0.createDump;
+   assign Halt = DUT.p0.memorySection.createDump;
    // Is processor halted (1 bit signal)
    
    /* Add anything else you want here */
