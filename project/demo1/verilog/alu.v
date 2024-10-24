2e4629cf1c9aac4bf70e75ff72c8ac9ddbc75a6e
agsiv25 <sivarajah@wisc.edu>
2024-10-24 14:18:08 -0500

    f

--- project/demo1/verilog/alu.v
+++ project/demo1/verilog/alu.v
@@ -75,8 +75,8 @@ module alu (InA, InB, Cin, Oper, invA, invB, sign, Out, Zero, Ofl, Cout, signFla
 
 	// 2's compliment ADD
 	cla_16b ALUadder (.sum(ADDout), .c_out(Cout), .ofl(Ofl), .a(Atouse), .b(Btouse), .c_in(Cin), .sign(sign));
-	always @(ADDout, Atouse, Btouse, Cin, invA) begin
-		$display("ALUadder: %d, %b, %d, %b", ADDout, Atouse, Btouse, Cin, invA);
+	always @(ADDout, Atouse, Btouse, Cin, invA, InA) begin
+		$display("ALUadder: %d, %b, %d, %b, %b", ADDout, Atouse, Btouse, Cin, invA, InA);
 	end
 
 	// Bitwise AND
