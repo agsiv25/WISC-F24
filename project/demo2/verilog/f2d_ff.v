0a4d67f02e74ea8fed31640c5c92f905abf0c02f
Arun Sivarajah <sivarajah@royal-21.cs.wisc.edu>
2024-11-06 12:31:54 -0600

    fetch to decode latch

---
+++ project/demo2/verilog/f2d_ff.v
@@ -0,0 +1,26 @@
+/*
+   CS/ECE 552 Spring '22
+  
+   Filename        : fetch.v
+   Description     : This is the flip flop between the fetch and decode cycles.
+*/
+`default_nettype none
+module fetch (instructionF, incPCF, errF, clk, rst, instructionD, wbDataD);
+
+input wire [15:0] instructionF;
+input wire [15:0] incPCF;
+input wire errF;
+input wire clk;
+input wire rst;
+
+output wire [15:0] instructionD;
+output wire [15:0] incPCD;
+
+dff instructLatch[15:0](.Q(instructionD), .D(instructionF), .clk(clk), .rst(rst));
+
+dff incPCLatch[15:0](.Q(incPCD), .D(incPCF), .clk(clk), .rst(rst));
+
+
+   
+endmodule
+`default_nettype wire
