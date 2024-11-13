972b09cf55208b12d0b25bf62cedc7d75b155d54
agsiv25 <sivarajah@wisc.edu>
2024-11-13 14:19:30 -0600

    f

--- project/demo2/verilog/hazard_det.v
+++ project/demo2/verilog/hazard_det.v
@@ -155,9 +155,9 @@ always @(*) begin
             branchInstF = 1'b1;
 
             rsHazard = (((fetch_inst[10:8] == wrtRegD) && regWrtD) || ((fetch_inst[10:8] == wrtRegX) && regWrtX) || ((fetch_inst[10:8] == wrtRegM) && regWrtM) || ((fetch_inst[10:8] == wrtRegW) && regWrtW)) ? 1'b1 : 1'b0; 
-            pcNop = rsHazard || branchInstD || branchInstX || branchInstM || branchInstW || branchInstF;
+            pcNop = rsHazard;
 
-            next_inst = fetch_inst;
+            next_inst = (pcNop || rst) ? NOP : fetch_inst;
         end
 
         // Only reads from RS and no control hazards
