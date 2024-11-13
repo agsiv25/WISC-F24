4e36ecc2ebd5a9849f7d6608f37f73f2d31e20ef
agsiv25 <sivarajah@wisc.edu>
2024-11-13 15:05:04 -0600

    f

--- project/demo2/verilog/fetch.v
+++ project/demo2/verilog/fetch.v
@@ -49,7 +49,7 @@ assign instrValid = 1'b1;
 
 cla_16b pc_inc(.sum(incPC), .c_out(), .ofl(pcIncErr), .a(pcRegAddr), .b(16'h2), .c_in(1'b0), .sign(1'b0));
 
-assign pcIfBranch = (branchInstW) ? newPC : incPC;
+assign pcIfBranch = (branchInstX) ? newPC : incPC;
 
 reg16 PC(.readData(pcRegAddr), .err(pcRegErr), .clk(clk), .rst(rst), .writeData(pcIfBranch), .writeEn(~createDump & ~pcNop));
 
