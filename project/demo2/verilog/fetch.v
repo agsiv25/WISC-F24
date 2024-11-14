63c4b43b3a3bb15f7980b70e86f4aae16968f51c
agsiv25 <sivarajah@wisc.edu>
2024-11-14 15:39:29 -0600

    g

--- project/demo2/verilog/fetch.v
+++ project/demo2/verilog/fetch.v
@@ -60,8 +60,6 @@ assign err = pcRegErr | pcIncErr;
 memory2c instruction_memory(.data_out(instruction2), .data_in(16'b0), .addr(pcRegAddr), .enable(1'b1), .wr(1'b0), .createdump(createDump), .clk(clk), .rst(rst));
    
 hazard_det hazard(.rst(rst), .clk(clk), .fetch_inst(instruction2), .next_inst(instruction), .pcNop(pcNop), .regWrtD(regWrtD), .regWrtX(regWrtX), .regWrtM(regWrtM), .regWrtW(regWrtW), .wrtRegD(wrtRegD), .wrtRegX(wrtRegX), .wrtRegM(wrtRegM), .wrtRegW(wrtRegW), .branchInstF(branchInstF), .branchInstD(branchInstD), .branchInstX(branchInstX), .branchInstM(branchInstM), .branchInstW(branchInstW));
-always @(posedge clk) begin
-    $display("Fetch: incPC: %h newPC: %h pcIfBranch: %h PC to read: %h, Instruction: %h", incPC, newPC, pcIfBranch, pcRegAddr, instruction);
-end
+
 endmodule
 `default_nettype wire
