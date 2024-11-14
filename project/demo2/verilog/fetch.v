bc83c5c96fc969cc6a9acd077f0739329bf149ad
agsiv25 <sivarajah@wisc.edu>
2024-11-13 19:52:56 -0600

    f

--- project/demo2/verilog/fetch.v
+++ project/demo2/verilog/fetch.v
@@ -49,9 +49,9 @@ assign instrValid = 1'b1;
 
 cla_16b pc_inc(.sum(incPC), .c_out(), .ofl(pcIncErr), .a(pcRegAddr), .b(16'h2), .c_in(1'b0), .sign(1'b0));
 
-assign pcIfBranch = (branchInstM) ? newPC : incPC;
+assign pcIfBranch = (branchInstX) ? newPC : incPC;
 
-reg16 PC(.readData(pcRegAddr), .err(pcRegErr), .clk(clk), .rst(rst), .writeData(pcIfBranch), .writeEn(~createDump & ~(pcNop & ~branchInstM)));
+reg16 PC(.readData(pcRegAddr), .err(pcRegErr), .clk(clk), .rst(rst), .writeData(pcIfBranch), .writeEn(~createDump & ~(pcNop & ~branchInstX)));
 
 // assign error signal to be an OR between the PC adder and the PC register
 assign err = pcRegErr | pcIncErr;
