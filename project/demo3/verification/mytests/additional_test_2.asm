79210a9529d33723ec2e448b6a64eb76c2d1f8f8
eman60 <evanwiedemeier@gmail.com>
2024-12-11 10:17:12 -0600

    test2

--- project/demo3/verification/mytests/additional_test_2.asm
+++ project/demo3/verification/mytests/additional_test_2.asm
@@ -5,8 +5,9 @@
 
 // Tests a simple hazard without NOPs inserted manually. 
 lbi r1, 0xF0 //r1 = 0xFFF0
-lbi r2, 0x0F //r2 = 0x000F
+//lbi r2, 0x0F //r2 = 0x000F
+NOP
 lbi r5, 0x11 // r2 to decode 
 xor r4, r5, r1
-add r5, r4, r4
+// add r5, r4, r4
 halt
