d07796fe1c4de2b88240b91c0e33bcd7a179819f
eman60 <evanwiedemeier@gmail.com>
2024-12-10 20:56:35 -0600

    add demo2 memory

--- project/demo3/verilog/memory.v
+++ project/demo3/verilog/memory.v
@@ -6,9 +6,22 @@
                      processor.
 */
 `default_nettype none
-module memory (/* TODO: Add appropriate inputs/outputs for your memory stage here*/);
+module memory (dataAddr, wrtData, memWrt, createDump, clk, rst, memOut, readEn);
 
    // TODO: Your code here
+
+   input wire [15:0]dataAddr;
+   input wire [15:0]wrtData;
+   input wire memWrt;
+   input wire createDump;
+   input wire clk;
+   input wire rst;
+   input wire readEn;
+
+   output wire [15:0]memOut;
+
+
+   memory2c data_mem(.data_out(memOut), .data_in(wrtData), .addr(dataAddr), .enable(~createDump), .wr(memWrt), .createdump(createDump), .clk(clk), .rst(rst));
    
 endmodule
 `default_nettype wire
