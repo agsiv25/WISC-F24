b320faf5e06a3ca53899a28e3b0403fd9f630890
eman60 <evanwiedemeier@gmail.com>
2024-12-11 18:33:08 -0600

    test2

--- project/demo3/verification/mytests/additional_test_2.asm
+++ project/demo3/verification/mytests/additional_test_2.asm
@@ -4,10 +4,9 @@
 
 
 // Tests a simple hazard without NOPs inserted manually. 
-lbi r1, 0xff    //expected value in r1 = -1
-lbi r2, 1       //expected value in r2 = 1
-sco r3, r1, r2  //expected value in r3 = 1
-sco r3, r2, r3  //expected value in r3 = 0
-sco r4, r3, r1  //expected value in r4 = 0
-halt
+lbi r0, U.Here
+slbi r0, L.Here   // r0 contains address of ".Here"
+lbi r1, 15
+st r1, r0, 0      // .Here should have the value 15
+ld r2, r0, 0      // r2 = 15
 halt
