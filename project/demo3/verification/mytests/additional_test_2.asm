677fee7b30f0f23c3e73f318f16172e52ed16f0f
eman60 <evanwiedemeier@gmail.com>
2024-12-11 20:58:26 -0600

    test2

--- project/demo3/verification/mytests/additional_test_2.asm
+++ project/demo3/verification/mytests/additional_test_2.asm
@@ -7,6 +7,7 @@
 lbi  r1, 0     //mem location tests first mem location
 lbi  r2, 8      //value to store
 lbi  r3, 0      //clear r3
+NOP
 st   r2, r1, 0  //store at mem location
 ld   r3, r1, 0  //load from mem location
 halt
