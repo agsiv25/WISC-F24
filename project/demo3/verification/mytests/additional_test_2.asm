30576c45b1cb34aa12aa08605023d1816eb8e408
eman60 <evanwiedemeier@gmail.com>
2024-12-11 20:52:48 -0600

    test2

--- project/demo3/verification/mytests/additional_test_2.asm
+++ project/demo3/verification/mytests/additional_test_2.asm
@@ -4,9 +4,9 @@
 
 
 // Tests a simple hazard without NOPs inserted manually. 
-lbi r0, 0
-slbi r0, 0x0B   // r0 contains address of ".Here"
-lbi r1, 15
-st r1, r0, 0      // .Here should have the value 15
-//ld r2, r0, 0      // r2 = 15
+lbi  r1, 0     //mem location tests first mem location
+lbi  r2, 8      //value to store
+lbi  r3, 0      //clear r3
+st   r2, r1, 0  //store at mem location
+ld   r3, r1, 0  //load from mem location
 halt
