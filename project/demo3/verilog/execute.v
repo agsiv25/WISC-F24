c8ce73437f1392de360d38bcc21b87004d4ec144
eman60 <evanwiedemeier@gmail.com>
2024-12-12 11:03:26 -0600

    use stuSel for wrtDataOut

--- project/demo3/verilog/execute.v
+++ project/demo3/verilog/execute.v
@@ -71,7 +71,9 @@ module execute (SLBIsel, incPC, immSrc, imm8, imm11, brchSig, Cin, inA, inB, inv
    //assign wrtDataXout = (stuSel) ? wrtDataXin : preSTForwardedInB;
    // for store 
    //assign wrtDataXout = (stuSel) ? preSTForwardedInB : wrtDataXin;
-   assign wrtDataXout = ~fwCntrlB[5] ? (fwCntrlB[4] ? preSTForwardedInB : wrtDataXin) : (fwCntrlB[4] ? wrtDataXin : preSTForwardedInB);
+   // assign wrtDataXout = ~fwCntrlB[5] ? (fwCntrlB[4] ? preSTForwardedInB : wrtDataXin) : (fwCntrlB[4] ? wrtDataXin : preSTForwardedInB);
+
+   wrtDataXout = (stuSel) ? wrtDataXin : preSTForwardedInB;
 
    assign forwardedInB = (stuSel) ? inB : preSTForwardedInB;
 
