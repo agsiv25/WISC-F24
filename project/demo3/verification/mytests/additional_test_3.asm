16e912d67534b8e86054f20afdd609c5a4ba0b30
eman60 <evanwiedemeier@gmail.com>
2024-12-16 13:17:17 -0600

    add test 3 for branch prediction

---
+++ project/demo3/verification/mytests/additional_test_3.asm
@@ -0,0 +1,13 @@
+// Original test: ./eharris/hw4/problem6/xor_2.asm
+// Author: eharris
+// Test source code follows
+
+
+// Tests BEQZ functionality with branch prediction. 
+lbi  r0, 0      // set to 0 so branch is taken
+beqz r0, 0x06
+lbi  r1, 0x0A
+lbi  r2, 0x0B
+halt
+lbi  r5, 0xFF  //only load if branch is taken
+halt
