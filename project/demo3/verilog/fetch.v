307d6c35268f5a60acc7e5f27f316ae0bb4ba2b3
eman60 <evanwiedemeier@gmail.com>
2024-12-16 22:14:32 -0600

    pc reg stall for NOP

--- project/demo3/verilog/fetch.v
+++ project/demo3/verilog/fetch.v
@@ -59,7 +59,7 @@ cla_16b pc_inc(.sum(incPC), .c_out(), .ofl(pcIncErr), .a(pcRegAddr), .b(16'h2), 
 
 assign pcIfBranch = (jumpInstX | branch_misprediction) ? newPC : incPC;
 
-reg16 PC(.readData(pcRegAddr), .err(pcRegErr), .clk(clk), .rst(rst), .writeData(pcIfBranch), .writeEn(~createDump & ~(pcNop & ~(jumpInstX | branch_misprediction))));
+reg16 PC(.readData(pcRegAddr), .err(pcRegErr), .clk(clk), .rst(rst), .writeData(pcIfBranch), .writeEn(~createDump & ~(pcNop & ~(jumpInstX | branch_misprediction | stall | ~(icacheErr === 1'b0)))));
 
 // assign error signal to be an OR between the PC adder and the PC register
 assign err = pcRegErr | pcIncErr | icacheErr;
