1d320650f3888c30a875a8bab3bf304e7e1043d8
eman60 <evanwiedemeier@gmail.com>
2024-12-17 18:01:29 -0600

    test 5

--- project/demo3/verification/mytests/additional_test_5.asm
+++ project/demo3/verification/mytests/additional_test_5.asm
@@ -1,30 +1,6 @@
-lbi r3, 0
-beqz r3, .GoHere
-halt
-halt
-halt
-halt
-
-.GoHere:
-ld r3, r2, 0       // r3 = 0x005e
-bnez r3, .GoThere
-halt
-halt
-halt
-halt
-
-.GoThere:
-lbi r0, U.GoGoGo
-slbi r0, L.GoGoGo
-jalr r0, 0
-.RetAddr:
-halt
-halt
-halt
-halt
-
-.GoGoGo:
-st r7, r2, -2      // .Data1-2 = .RetAddr (0x00a4)
-halt
-halt
-halt
+lbi r0, 8
+lbi r1, 0
+st r0, r1, 0
+NOP
+NOP
+ld r2, r1, 0
